1550038e04bfab547770e92d0855632adc2a3440
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-07-23 09:13:44 +0800

    add new pages

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,14 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/generals-quaters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+
+	fileserver := http.FileServer(http.Dir("./assets/"))
+	mux.Handle("/assets/*", http.StripPrefix("/assets", fileserver))
 
 	return mux
 }
