35d3d92ae9b6607e8a32b28d6f26ace1442af464
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-07-26 23:58:24 +0800

    pass data through session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/Yapcheekian/web-golang/internal/config"
 	"github.com/Yapcheekian/web-golang/internal/handlers"
+	"github.com/Yapcheekian/web-golang/internal/models"
 	"github.com/Yapcheekian/web-golang/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -18,7 +20,7 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
-
+	gob.Register(models.Reservation{})
 	app.IsProduction = false
 
 	session = scs.New()
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -81,6 +81,9 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		})
 		return
 	}
+
+	m.App.Session.Put(r.Context(), "reservation", reservation)
+	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
 
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
@@ -120,4 +123,23 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
+}
+
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+
+	if !ok {
+		log.Println("cannot get item from session")
+		m.App.Session.Put(r.Context(), "error", "can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	m.App.Session.Remove(r.Context(), "reservation")
+
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+	render.RenderTemplate(w, r, "summary-reservation.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
