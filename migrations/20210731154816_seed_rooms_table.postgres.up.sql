5c66ca1e559c207f451eac85ceb2de2d40d0a5fa
Yap Chee Kian <yapcheekian@Yaps-MacBook-Pro.local>
2021-07-31 23:54:29 +0800

    add sample data

---
+++ migrations/20210731154816_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO rooms(room_name, created_at, updated_at)
+VALUES ('General Quater', '2021-07-30 00:00:00.000', '2021-07-30 00:00:00.000'),
+('Major Suite', '2021-07-30 00:00:00.000', '2021-07-30 00:00:00.000');
---
+++ migrations/20210731155217_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO restrictions(restrictions, created_at, updated_at)
+VALUES ('Reservation', '2021-07-30 00:00:00.000', '2021-07-30 00:00:00.000'),
+('Owner Block', '2021-07-30 00:00:00.000', '2021-07-30 00:00:00.000')
